874138204712c10ecf7333bee9ee8aec0f1bbc80
J <jbtuason@gmail.com>
2016-07-17 12:11:38 -0400

    Initial

---
+++ Assets/Resources/Scripts/PickupObjects.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObjects : MonoBehaviour {
+
+    private bool isOverlapping;
+    private Collider otherObj;
+    private GameObject holdingUI;
+
+	// Use this for initialization
+	void Start () {
+        holdingUI = gameObject.transform.GetChild(0).GetChild(2).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("space"))
+        {
+            if (isOverlapping)
+            {
+                Destroy(otherObj.gameObject);
+                holdingUI.SetActive(true);
+            }
+            
+        }
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        isOverlapping = true;
+        otherObj = other;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        isOverlapping = false;
+    }
+
+}
